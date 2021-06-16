ddcfbe5f210f32e48cb81f52574ad77b5e43f847
aaron-might <aaronmighty7@gmail.com>
2021-06-15 23:20:05 -0400

    writing require route file and setup listener

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+//Require routes file
+require('./routes/routes')(app);
+
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
